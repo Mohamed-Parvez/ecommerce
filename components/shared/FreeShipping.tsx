2c15fa4a6805ff4168490248994ad50b1f1d9af2
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-02-02 22:39:23 +0530

    updated font

--- components/shared/FreeShipping.tsx
+++ components/shared/FreeShipping.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const FreeShipping = () => {
   return (
-    <main className="mx-auto max-w-2xl py-1">
+    <section className="mx-auto max-w-2xl py-1">
       <p className="extra-small text-center text-black">
         Free shipping for orders over Rs.500
       </p>
-    </main>
+    </section>
   );
 };
 
---
+++ components/ui/logo.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import React from 'react'
+
+const logo = () => {
+  return (
+    <main>
+      <Image 
+        src={""}
+        alt=''
+        height={20}
+        width={20}
+      />
+    </main>
+  )
+}
+
+export default logo
