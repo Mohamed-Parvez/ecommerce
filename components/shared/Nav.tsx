f484787c6af68da55c06f0cc9e4c19ee1e97c957
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-02-16 23:22:01 +0530

    Add menu

--- components/shared/Nav.tsx
+++ components/shared/Nav.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import Menu from "../ui/menu";
+import Logo from "../ui/logo";
 
 const Nav = () => {
   return (
-    <section>
-      Nav
+    <section className="items-top flex space-x-2 bg-black px-2 py-20">
+      <div className="mt-[-8px] block md:hidden">
+        <Menu
+          src={"/assets/icons/bars_sm.svg"}
+          alt="bars"
+          height={28}
+          width={28}
+        />
+      </div>
+      <div className="hidden md:block">
+        <Menu
+          src={"/assets/icons/bars_lg.svg"}
+          alt="bars"
+          height={28}
+          width={28}
+        />
+      </div>
+      <div>
+        <Logo
+          src={"/assets/icons/logo.svg"}
+          alt="logo"
+          height={40}
+          width={99}
+        />
+      </div>
     </section>
   );
 };
---
+++ components/ui/logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ImageProps } from "@/types";
+import Image from "next/image";
+
+const Logo = ({ src, alt, height, width }: ImageProps) => {
+  return (
+    <main>
+      <Image src={src} alt={alt} height={height} width={width} />
+    </main>
+  );
+};
+
+export default Logo;
